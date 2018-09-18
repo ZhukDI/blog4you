ec0606f6d8d5d784794964ece222224b4d6f1282
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2018-09-18 21:18:32 +0300

    Added some service methods and XSD file. Configured jaxb2-maven-plugin

--- src/main/java/com/zhukdi/blog4you/service/IPostService.java
+++ src/main/java/com/zhukdi/blog4you/service/IPostService.java
@@ -1,7 +1,16 @@
 package com.zhukdi.blog4you.service;
 
+import com.zhukdi.blog4you.entity.Post;
+
+import java.util.List;
+
 /**
  * Created by Dmitry on 9/17/2018.
  */
 public interface IPostService {
+    List<Post> getAllPosts();
+    Post getPostById(Long id);
+    boolean addPost(Post post);
+    void updatePost(Post post);
+    void deletePost(Post post);
 }
--- src/main/java/com/zhukdi/blog4you/service/PostServiceImpl.java
+++ src/main/java/com/zhukdi/blog4you/service/PostServiceImpl.java
@@ -1,7 +1,49 @@
 package com.zhukdi.blog4you.service;
 
+import com.zhukdi.blog4you.entity.Post;
+import com.zhukdi.blog4you.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by Dmitry on 9/18/2018.
  */
+@Service
 public class PostServiceImpl implements IPostService {
+    @Autowired
+    private PostRepository postRepository;
+
+
+    @Override
+    public Post getPostById(Long id) {
+        Optional<Post> posts = postRepository.findById(id);
+//        Post post = postRepository.findById(id);
+        return posts.orElse(null);
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        List<Post> posts = new ArrayList<>();
+        postRepository.findAll().forEach(posts::add);
+        return posts;
+    }
+
+    @Override
+    public boolean addPost(Post post) {
+        return false;
+    }
+
+    @Override
+    public void updatePost(Post post) {
+
+    }
+
+    @Override
+    public void deletePost(Post post) {
+
+    }
 }
