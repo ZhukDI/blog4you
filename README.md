bf0a9d4119f39856b8f441e1ae188bf410a1a53f
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2018-09-30 13:10:34 +0300

    Added swagger ui

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blog4you
+
+### Swagger
+Api Documentation:
+https://localhost:8087/swagger-ui.html
--- src/main/java/com/zhukdi/blog4you/BlogApplication.java
+++ src/main/java/com/zhukdi/blog4you/BlogApplication.java
@@ -9,7 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by Dmitry on 9/17/2018.
@@ -20,6 +26,19 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+
+    @Configuration
+    @EnableSwagger2
+    public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 
 //    @Bean
