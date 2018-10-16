d4c726534c9c70309840d870c695c536bfb3eadb
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2018-10-17 00:24:44 +0300

    Modified comment and post components

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostService } from "./post/post.service";
 import { CommentComponent } from './comment/comment.component';
+import { CommentFormComponent } from './comment-form/comment-form.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    CommentComponent
+    CommentComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
